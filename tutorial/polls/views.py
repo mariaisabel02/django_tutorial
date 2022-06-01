66592b98b3fa2378a3c52e0895e3c8bf4b5bd1c0
mariaisabel02 <mimurillo@primarix.com>
2022-06-01 09:39:00 -0600

    adds 404 error on detail page

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -9,7 +9,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
